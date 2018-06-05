b8c9ba97cf4efddad1e5d55b2f0b6e9e90a03c2a
gio.yan <gio.yan@motionglobal.com>
2018-06-05 10:12:23 +0800

    design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Personal Website</title>
-
     <link href="//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
     <script src="//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -17,20 +16,60 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-
-                    <div class="panel-body">
-                        <a href="{{ url('articles-lists') }}" class="btn btn-lg btn-success col-xs-12">搜索文章</a>
+    <div>
+        <!-- Carousel -->
+        <div id="templatemo-carousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#alatemo-carousel" data-slide-to="0" class="active"></li>
+                <li data-target="#templatemo-carousel" data-slide-to="1"></li>
+                <li data-target="#templatemo-carousel" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1>PERSONAL ASSISTENT WEBSITE</h1>
+                            <p>START WITH YOUR THOUGHTS</p>
+                            <p><a class="btn btn-lg btn-green" href="/articles-lists" role="button" style="margin: 20px;">Write Article</a>
+                                <a class="btn btn-lg btn-orange" href="/issues-deadline" role="button">Issues Remind</a></p>
+                        </div>
                     </div>
-                    <div class="panel-body">
-                        <a href="{{ url('issues-deadline') }}" class="btn btn-lg btn-success col-xs-12">查看事务</a>
-                    </div>
-
                 </div>
+
+                <div class="item">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <div class="col-sm-6 col-md-6">
+                                <h1>ARTICLES</h1>
+                                <p>Our website provides powerful text editor so that you can store everthing in your article and share your idea.</p>
+                            </div>
+                            <div class="col-sm-6 col-md-6">
+                                <h1>ISSUES</h1>
+                                <p>Our website provides free mail service and remind you of your important issues every day.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="container">
+                        <div class="carousel-caption">
+                            <h1>Open Source</h1>
+                            <p>This website can be used for everyone.</p>
+                            <p><a class="btn btn-lg btn-orange" href="https://github.com/Yan5zpky/personal_assitant" target="_blank" role="button">Read More</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="left carousel-control" href="#templatemo-carousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+            <a class="right carousel-control" href="#templatemo-carousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+        </div><!-- /#templatemo-carousel -->
+    </div>
+    <div class="templatemo-welcome" id="templatemo-welcome">
+        <div class="container">
+            <div class="templatemo-slogan text-center">
+                <span class="txt_darkgrey">Welcome to </span><span class="txt_orange">Laravel</span>
+                <p class="txt_slogan"><i>Share you idea with everyone.</i></p>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,48 +21,126 @@
         //    startDate: '-3d'
         // });
     </script>
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <!--
+
+    Urbanic Template
+
+    http://www.templatemo.com/tm-395-urbanic
+
+    -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel="shortcut icon" href="PUT YOUR FAVICON HERE">-->
+
+    <!-- Google Web Font Embed -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="js/colorbox/colorbox.css"  rel='stylesheet' type='text/css'>
+    <link href="css/templatemo_style.css"  rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+    <script src="js/stickUp.min.js"  type="text/javascript"></script>
+    <script src="js/colorbox/jquery.colorbox-min.js"  type="text/javascript"></script>
+    <script src="js/templatemo_script.js"  type="text/javascript"></script>
+    <![endif]-->
 </head>
 <body id="app-layout">
-<nav class="navbar navbar-default navbar-static-top">
+<div class="templatemo-top-bar" id="templatemo-top">
     <div class="container">
-        <div class="navbar-header">
-
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('/') }}">Home</a></li>
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                        </ul>
-                    </li>
-                @endif
-            </ul>
+        <div class="subheader">
+            <div id="phone" class="pull-left">
+                <img src="images/phone.png" alt="phone"/>
+                +86 19921030137
+            </div>
+            <div id="email" class="pull-right">
+                <img src="images/email.png" alt="email"/>
+                yanloveky@gmail.com
+            </div>
         </div>
     </div>
-</nav>
+</div>
+<div class="templatemo-top-menu">
+    <div class="container">
+        <!-- Static navbar -->
+        <div class="navbar navbar-default" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a href="#" class="navbar-brand"><img src="images/templatemo_logo.png" alt="Urbanic Template" title="Urbanic Template" /></a>
+                </div>
+                <div class="navbar-collapse collapse" id="templatemo-nav-bar">
+                    <ul class="nav navbar-nav navbar-right" style="margin-top: 40px;">
+                        <li><a href="/">HOME</a></li>
+                        @if (Auth::guest())
+                        <li><a href="/login">LOGIN</a></li>
+                        <li><a href="/register">REGISTER</a></li>
+                        @else
+                        <li><a href="/articles-lists">ARTICLES</a></li>
+                        <li><a href="/issues-deadline">ISSUES</a></li>
+                        <li><a href="/logout">LOG OUT</a></li>
+                        @endif
+                        <li><a href="https://github.com/Yan5zpky/personal_assitant">GITHUB</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div><!--/.container-fluid -->
+        </div><!--/.navbar -->
+    </div> <!-- /container -->
+</div>
+{{--<nav class="navbar navbar-default navbar-static-top">--}}
+    {{--<div class="container">--}}
+        {{--<div class="navbar-header">--}}
+
+            {{--<!-- Collapsed Hamburger -->--}}
+            {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">--}}
+                {{--<span class="sr-only">Toggle Navigation</span>--}}
+                {{--<span class="icon-bar"></span>--}}
+                {{--<span class="icon-bar"></span>--}}
+                {{--<span class="icon-bar"></span>--}}
+            {{--</button>--}}
+        {{--</div>--}}
+
+        {{--<div class="collapse navbar-collapse" id="app-navbar-collapse">--}}
+            {{--<!-- Left Side Of Navbar -->--}}
+            {{--<ul class="nav navbar-nav">--}}
+                {{--<li><a href="{{ url('/') }}">Home</a></li>--}}
+            {{--</ul>--}}
+
+            {{--<!-- Right Side Of Navbar -->--}}
+            {{--<ul class="nav navbar-nav navbar-right">--}}
+                {{--<!-- Authentication Links -->--}}
+                {{--@if (Auth::guest())--}}
+                    {{--<li><a href="{{ url('/login') }}">Login</a></li>--}}
+                    {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
+                {{--@else--}}
+                    {{--<li class="dropdown">--}}
+                        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
+                            {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
+                        {{--</a>--}}
+
+                        {{--<ul class="dropdown-menu" role="menu">--}}
+                            {{--<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                {{--@endif--}}
+            {{--</ul>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</nav>--}}
 
 @yield('content')
 
